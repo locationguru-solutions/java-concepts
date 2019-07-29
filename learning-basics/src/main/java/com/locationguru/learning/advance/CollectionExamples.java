be79a07d93789460908753b7ef090c238d3ab02c
Kedar Joshi <kdar_joshi@yahoo.com>
2019-07-29 14:17:06 +0530

    Prepared for concurrent collections

---
+++ learning-basics/src/main/java/com/locationguru/learning/advance/CollectionExamples.java
@@ -0,0 +1,15 @@
+package com.locationguru.learning.advance;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CollectionExamples
+{
+	private static final Logger logger = LogManager.getLogger(CollectionExamples.class);
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
